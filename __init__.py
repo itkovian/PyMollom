264d59c5c065af6d36aaa26fbccb3eb22937a006
Andy Georges <itkovian@gmail.com>
2012-04-25 08:49:30 +0200

    Added error classes.

--- __init__.py
+++ __init__.py
@@ -32,3 +32,117 @@ __date__ = 'April 24, 2012'
 import API.Content as Content
 
 __all__ = [Content]
+
+
+MOLLOM_SERVER="http://rest.mollom.com/"
+MOLLOM_VERSION="v1"
+
+
+class MollomError(Exception):
+    """Base class for all Mollom errors.
+
+    In general, the following status codes result in errors:
+
+    401 - Unauthorised
+    403 - Forbidden
+    404 - Not Found
+
+    These errors may have more specific meaning when used in
+    the different APIs.
+    """
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
+
+    def __str__(self):
+        return "Mollom error [%d] - %s" % (self.code, self.message)
+
+
+class ConnectionError(MollomError):
+    pass
+
+class BlacklistError(MollomError):
+    pass
+
+
+class CaptchaError(MollomError):
+    CAPTCHA_DOES_NOT_EXIST = 404
+    CAPTCHA_ALREADY_PROCESSED = 409
+    CAPTCHA_EXPIRED = 410
+
+    def __init__(self, code, message):
+        super(CaptchaError, self).__init__(code, message)
+
+class CaptchaDoesNotExistError(CaptchaError):
+    def __init__(self, code, message):
+        super(CaptchaDoesNotExist, self).__init__(code, message)
+
+class CaptchaAlreadyProcessedError(CaptchaError):
+    def __init__(self, code, message):
+        super(CaptchaAlreadyProcessed, self).__init__(code, message)
+
+class CaptchaExpiredError(CaptchaError):
+    def __init__(self, code, message):
+        super(CaptchaExpired, self).__init__(code, message)
+
+
+class ContentError(MollomError):
+    def __init__(self, code, message):
+        super(ContentError, self).__init__(code, message)
+
+
+class FeedbackError(MollomError):
+    FEEDBACK_MISSING_ID = 400
+    FEEDBACK_UNKNOWN_REASON = 400
+
+    def __init__(self, code, message)
+        super(FeedbackError, self).__init__(code, message)
+
+class FeedbackMissingIdError(FeedbackError):
+    def __init__(self, code, message):
+        super(FeedbackMissingIdError, self).__init__(code, message)
+
+class FeedbackUnknownReasonError(FeedbackError):
+    def __init__(self, code, message):
+        super(FeedbackUnknownReasonError, self).__init__(code, message)
+
+class SiteError(MollomError):
+    SITE_UNKNOWN = 404
+
+    def __init__(self, code, message):
+        super(SiteError, self).__init__(code, message)
+
+
+class SiteUnknownError(SiteError):
+    def __init__(self, code, message):
+        super(SiteUnknownError, self).__init__(code, mesage)
+
+
+
+class WhitelistError(MollomError):
+    WHITELIST_ENTRY_UNKNOW
+
+    def __init__(self, code, message):
+        super(WhitelistError, self).__init__(code, message)
+
+
+class WhitelistUnknownEntryError(WhitelistError):
+    def __init__(self, code, message):
+        super(WhitelistUnknownEntryError, self).__init__(code, message)
+
+
+class Unauthorised(MollomError):
+    pass
+
+class Forbidden(MollomError):
+    pass
+
+class NotFound(MollomError):
+    pass
+
+class JSONParseError(MollomError):
+    pass
+
+
+
+
